fb9220b7022ce354656f7d4e3a2e4e35ef3075d5
Lada Kostina <lboons413@gmail.com>
2024-12-06 00:18:25 +0300

    Firebase

--- TravelItinerary/app/build.gradle.kts
+++ TravelItinerary/app/build.gradle.kts
@@ -72,4 +72,5 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:$nav_version")
     implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.firebaseui:firebase-ui-auth:7.2.0")
 }
